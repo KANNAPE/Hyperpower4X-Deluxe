93b87a1712e8e16c9b042c8dfa02aa1942950d5c
KANNAPE <bergerthomas38@gmail.com>
2023-09-11 15:01:36 +0200

    First commit

---
+++ scripts/Board.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+const ROWS = 6
+const COLS = 7
+
+var width : int = 0
+var height : int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
